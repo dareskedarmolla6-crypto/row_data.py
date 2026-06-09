53742e8b2bfe0fa6e47775071068948fc2d1187d
dareskedarmolla6-crypto <dareskedarmolla6@gmail.com>
2026-06-09 13:18:14 +0300

    Add system configurations and API placeholders

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+# System configurations and API placeholders
+API_KEY = "YOUR_BINANCE_API_KEY"
+API_SECRET = "YOUR_BINANCE_API_SECRET"
+
+SYMBOL = "BTC/USDT"
+TIMEFRAME = "1h"
+
+# Trading mode: 'live' or 'paper' (testing)
+TRADING_MODE = "paper"
