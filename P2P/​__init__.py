ae88db198a18d42e4ecf5e09c4aeeef05dd2beb2
dareskedarmolla6-crypto <dareskedarmolla6@gmail.com>
2026-06-18 17:36:55 +0300

    Refactor __init__.py for P2P module structure

--- P2P/​__init__.py
+++ P2P/​__init__.py
@@ -1,1 +1,20 @@
-Edcgfnter
+# FSE P2P Module (Core Entry Point)
+
+from .p2p_api import P2PAPIConnector
+from .p2p_scanner import P2PScanner
+from .p2p_risk import P2PRiskEngine
+from .p2p_executor import P2PExecutor
+from .p2p_wallet_manager import P2PWalletManager
+
+# 🔥 Optional (recommended next layer)
+from .p2p_orchestrator import P2POrchestrator
+
+
+__all__ = [
+    "P2PAPIConnector",
+    "P2PScanner",
+    "P2PRiskEngine",
+    "P2PExecutor",
+    "P2PWalletManager",
+    "P2POrchestrator"
+]
